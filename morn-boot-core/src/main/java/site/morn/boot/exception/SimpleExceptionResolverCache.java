294b6c09bd7a9f05338e3c7285f6c5c97dba60f1
timely-rain <lolic_sd@hotmail.com>
2018-10-14 11:58:25 +0800

    合并异常解释器缓存接口

--- morn-boot-core/src/main/java/site/morn/boot/exception/SimpleExceptionResolverCache.java
+++ morn-boot-core/src/main/java/site/morn/boot/exception/SimpleExceptionResolverCache.java
@@ -1,14 +1,11 @@
 package site.morn.boot.exception;
 
-import org.springframework.cache.annotation.Cacheable;
 import site.timely.exception.AnnotationExceptionResolver;
 import site.timely.exception.ExceptionResolver;
 import site.timely.exception.ExceptionResolverCache;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
-import java.util.stream.Collectors;
 
 /**
  * 默认异常处理器的缓存器
@@ -22,39 +19,42 @@ public class SimpleExceptionResolverCache implements ExceptionResolverCache {
     /**
      * 仓库
      */
-    private List<AnnotationExceptionResolver> store;
+    private List<AnnotationExceptionResolver> store = new ArrayList<>();
 
-    public SimpleExceptionResolverCache() {
-        store = new Vector<>();
-    }
-
-    @Cacheable(value = DEFAULT_CACHE, key = "'TAG.'+T(org.springframework.util.StringUtils).arrayToCommaDelimitedString(#tag)")
+    //    @Cacheable(value = DEFAULT_CACHE, key = "'TAG.'+T(org.springframework.util.StringUtils).arrayToCommaDelimitedString(#tag)")
     @Override
     public List<ExceptionResolver> find(String tag) {
-        return store.stream().filter(resolver -> Arrays.binarySearch(resolver.getTags(), tag) >= 0).collect(Collectors.toList());
+//        return store.stream().filter(resolver -> Arrays.binarySearch(resolver.getTags(), tag) >= 0).collect(Collectors.toList());
+        return null;
     }
 
-    @Cacheable(value = DEFAULT_CACHE, key = "'CLASS.'+T(org.springframework.util.StringUtils).arrayToCommaDelimitedString(#targets)")
+    //    @Cacheable(value = DEFAULT_CACHE, key = "'CLASS.'+T(org.springframework.util.StringUtils).arrayToCommaDelimitedString(#targets)")
     @Override
     public List<ExceptionResolver> find(Class<?>... targets) {
-        return store.stream().filter(resolver -> {
-            for (Class<?> resolverTarget : resolver.getTargets()) {
-                for (Class<?> target : targets) {
-                    if (target.isAssignableFrom(resolverTarget))
-                        return true;
-                }
-            }
-            return false;
-        }).collect(Collectors.toList());
+//        return store.stream().filter(resolver -> {
+//            for (Class<?> resolverTarget : resolver.getTargets()) {
+//                for (Class<?> target : targets) {
+//                    if (target.isAssignableFrom(resolverTarget))
+//                        return true;
+//                }
+//            }
+//            return false;
+//        }).collect(Collectors.toList());
+        return null;
     }
 
-    @Override
-    public void put(List<AnnotationExceptionResolver> resolvers) {
-        store.addAll(resolvers);
-    }
+//    @Override
+//    public void put(List<AnnotationExceptionResolver> resolvers) {
+//        store.addAll(resolvers);
+//    }
+
+//    @Override
+//    public void put(AnnotationExceptionResolver resolver) {
+//        store.add(resolver);
+//    }
 
     @Override
-    public void put(AnnotationExceptionResolver resolver) {
-        store.add(resolver);
+    public void put(List<?> resolvers) {
+        store.addAll((List<AnnotationExceptionResolver>) resolvers);
     }
 }
