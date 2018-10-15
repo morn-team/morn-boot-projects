e96ddbf99b324ec3c2714cede65d951a6d8c7967
timely-rain <lolic_sd@hotmail.com>
2018-10-15 23:28:15 +0800

    翻译器自动化配置

---
+++ morn-boot-autoconfigure/src/main/java/site/morn/boot/autoconfigure/TranslatorAutoConfiguration.java
@@ -0,0 +1,45 @@
+package site.morn.boot.autoconfigure;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import site.morn.boot.translate.DefaultSpringTranslator;
+import site.morn.boot.translate.TranslatorListener;
+import site.timely.translate.Translator;
+
+/**
+ * 翻译器自动化配置
+ *
+ * @author timely-rain
+ * @version 1.0.0, 2018/10/15
+ * @since 1.0
+ */
+@Configuration
+@ConditionalOnClass({Translator.class})
+@ConditionalOnProperty(prefix = "morn.translator", value = "enabled", havingValue = "true")
+public class TranslatorAutoConfiguration {
+
+  /**
+   * 注册翻译器
+   *
+   * @return 翻译器
+   */
+  @Bean
+  @ConditionalOnMissingBean
+  public Translator translator(MessageSource messageSource) {
+    return new DefaultSpringTranslator(messageSource);
+  }
+
+  /**
+   * 注册翻译监听器
+   *
+   * @return 翻译监听器
+   */
+  @Bean
+  public TranslatorListener translatorListener() {
+    return new TranslatorListener();
+  }
+}
