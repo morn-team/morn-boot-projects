c6fd563571bb46a77b365ca4ba61c73adcb87742
timely-rain <lolic_sd@hotmail.com>
2018-11-28 22:52:05 +0800

    注册标识实例后置处理器

--- morn-boot-autoconfigure/src/main/java/site/morn/boot/autoconfigure/BeanAutoConfiguration.java
+++ morn-boot-autoconfigure/src/main/java/site/morn/boot/autoconfigure/BeanAutoConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import site.morn.bean.IdentifiedBeanCache;
+import site.morn.boot.bean.IdentifiedBeanPostProcessor;
 import site.morn.boot.bean.SimpleIdentifiedBeanCache;
 
 /**
@@ -18,9 +19,31 @@ import site.morn.boot.bean.SimpleIdentifiedBeanCache;
 @ConditionalOnClass(CacheManager.class)
 public class BeanAutoConfiguration {
 
+  /**
+   * 注册标识实例缓存
+   *
+   * <p>提供实例缓存功能
+   *
+   * @return 标识实例缓存
+   */
   @Bean
   @ConditionalOnMissingBean
   public IdentifiedBeanCache identifiedBeanCache() {
     return new SimpleIdentifiedBeanCache();
+  }
+
+  /**
+   * 注册标识实例后置处理器
+   *
+   * <p>识别实例注解并缓存
+   *
+   * @param identifiedBeanCache 标识实例缓存
+   * @return 标识实例后置处理器
+   */
+  @Bean
+  @ConditionalOnMissingBean
+  public IdentifiedBeanPostProcessor identifiedBeanPostProcessor(
+      IdentifiedBeanCache identifiedBeanCache) {
+    return new IdentifiedBeanPostProcessor(identifiedBeanCache);
   }
 }
