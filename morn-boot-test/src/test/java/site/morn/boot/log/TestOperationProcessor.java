23fe746ac237761a4ad4886255f82326f5831975
timely-rain <lolic_sd@hotmail.com>
2018-12-05 22:42:17 +0800

    处理操作日志的单元测试

---
+++ morn-boot-test/src/test/java/site/morn/boot/log/TestOperationProcessor.java
@@ -0,0 +1,24 @@
+package site.morn.boot.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import site.morn.bean.annotation.Tag;
+import site.morn.log.Operation;
+import site.morn.log.OperationProcessor;
+
+/**
+ * 测试操作处理器
+ *
+ * @author timely-rain
+ * @since 1.0.0, 2018/12/5
+ */
+@Component
+@Slf4j
+@Tag
+public class TestOperationProcessor implements OperationProcessor {
+
+  @Override
+  public void accept(Operation operation) {
+    log.info("保存操作日志：" + operation);
+  }
+}
