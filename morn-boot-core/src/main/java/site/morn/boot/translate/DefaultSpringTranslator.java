813e18266501370e99ed35f5c426123752d40176
timely-rain <lolic_sd@hotmail.com>
2018-12-10 00:00:22 +0800

    实现按翻译载体生成国际化消息

--- morn-boot-core/src/main/java/site/morn/boot/translate/DefaultSpringTranslator.java
+++ morn-boot-core/src/main/java/site/morn/boot/translate/DefaultSpringTranslator.java
@@ -3,6 +3,9 @@ package site.morn.boot.translate;
 import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import site.morn.bean.IdentifiedBeanCache;
+import site.morn.translate.Transfer;
+import site.morn.translate.TranslateChanger;
 import site.morn.translate.Translator;
 
 /**
@@ -14,10 +17,20 @@ import site.morn.translate.Translator;
  */
 public class DefaultSpringTranslator implements Translator {
 
+  /**
+   * Spring国际化
+   */
   private final MessageSource messageSource;
 
-  public DefaultSpringTranslator(MessageSource messageSource) {
+  /**
+   * 实例缓存
+   */
+  private final IdentifiedBeanCache beanCache;
+
+  public DefaultSpringTranslator(MessageSource messageSource,
+      IdentifiedBeanCache beanCache) {
     this.messageSource = messageSource;
+    this.beanCache = beanCache;
   }
 
   @Override
@@ -28,5 +41,11 @@ public class DefaultSpringTranslator implements Translator {
   @Override
   public String translate(Locale locale, String code, Object... args) {
     return messageSource.getMessage(code, args, code, locale);
+  }
+
+  @Override
+  public <T> T translate(Transfer transfer, Class<T> cls) {
+    TranslateChanger<T> translateChanger = beanCache.bean(TranslateChanger.class, cls);
+    return translateChanger.change(transfer);
   }
 }
