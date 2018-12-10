7b3b5ea33606f29bcb2d231fdd75ff0318697543
timely-rain <lolic_sd@hotmail.com>
2018-12-10 22:39:05 +0800

    异常消息单元测试

---
+++ morn-boot-test/src/test/java/site/morn/exception/WarningsTest.java
@@ -0,0 +1,31 @@
+package site.morn.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 警告构建器单元测试
+ *
+ * @author timely-rain
+ * @since 1.0.0, 2018/12/10
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class WarningsTest {
+
+  @Test
+  public void message() {
+    ExceptionMessage message = Warnings.message("login.password-is-null", "timely");
+    Assert.assertNotNull("异常消息构建失败", message);
+    log.info(message.toString());
+    Assert.assertNotNull(message.getSolution());
+  }
+}
