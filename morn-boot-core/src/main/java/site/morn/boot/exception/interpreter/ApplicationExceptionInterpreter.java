84daa761371bd5e12a08833c53d7fe85499f2cc0
timely-rain <lolic_sd@hotmail.com>
2018-12-12 23:56:57 +0800

    新增应用异常解释器

---
+++ morn-boot-core/src/main/java/site/morn/boot/exception/interpreter/ApplicationExceptionInterpreter.java
@@ -0,0 +1,26 @@
+package site.morn.boot.exception.interpreter;
+
+import site.morn.bean.annotation.Target;
+import site.morn.exception.ApplicationException;
+import site.morn.exception.ApplicationMessage;
+import site.morn.exception.ExceptionInterpreter;
+
+/**
+ * 应用异常解释器
+ *
+ * <p>处理Morn相关异常
+ *
+ * @author timely-rain
+ * @since 1.0.0, 2018/12/12
+ */
+@Target(ApplicationException.class)
+public class ApplicationExceptionInterpreter implements ExceptionInterpreter {
+
+  @Override
+  public ApplicationMessage resolve(Throwable throwable) {
+    if (throwable instanceof ApplicationException) {
+      return ((ApplicationException) throwable).getApplicationMessage();
+    }
+    return null;
+  }
+}
