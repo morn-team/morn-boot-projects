3664813a537628f494cb6581a7853483a8d424aa
timely-rain <lolic_sd@hotmail.com>
2018-12-19 00:18:21 +0800

    REST初始化时，注入REST配置项

--- morn-boot-core/src/main/java/site/morn/boot/rest/RestInitializer.java
+++ morn-boot-core/src/main/java/site/morn/boot/rest/RestInitializer.java
@@ -1,7 +1,9 @@
 package site.morn.boot.rest;
 
 import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import site.morn.bean.IdentifiedBeanCache;
+import site.morn.rest.RestProperties;
 import site.morn.rest.Rests;
 import site.morn.translate.Translator;
 
@@ -11,20 +13,26 @@ import site.morn.translate.Translator;
  * @author timely-rain
  * @since 1.0.0, 2018/11/29
  */
+@RequiredArgsConstructor
 public class RestInitializer {
 
+  /**
+   * 实例缓存
+   */
   private final IdentifiedBeanCache identifiedBeanCache;
 
+  /**
+   * 翻译器
+   */
   private final Translator translator;
 
-  public RestInitializer(IdentifiedBeanCache identifiedBeanCache,
-      Translator translator) {
-    this.identifiedBeanCache = identifiedBeanCache;
-    this.translator = translator;
-  }
+  /**
+   * REST配置项
+   */
+  private final RestProperties restProperties;
 
   @PostConstruct
   public void initRest() {
-    Rests.initialize(identifiedBeanCache, translator);
+    Rests.initialize(identifiedBeanCache, translator, restProperties);
   }
 }
