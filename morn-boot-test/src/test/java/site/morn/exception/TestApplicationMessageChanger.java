8a83ce05d237d9961a9aa829effcd5bb6b078b22
timely-rain <lolic_sd@hotmail.com>
2018-12-21 00:32:24 +0800

    默认不开启测试应用消息转换器

--- morn-boot-test/src/test/java/site/morn/exception/TestApplicationMessageChanger.java
+++ morn-boot-test/src/test/java/site/morn/exception/TestApplicationMessageChanger.java
@@ -1,6 +1,5 @@
 package site.morn.exception;
 
-import org.springframework.stereotype.Component;
 import site.morn.bean.annotation.Target;
 import site.morn.translate.Transfer;
 import site.morn.translate.TranslateChanger;
@@ -11,7 +10,7 @@ import site.morn.translate.TranslateChanger;
  * @author timely-rain
  * @since 1.0.0, 2018/12/11
  */
-@Component
+//@Component
 @Target(ApplicationMessage.class)
 public class TestApplicationMessageChanger implements TranslateChanger<ApplicationMessage> {
 
