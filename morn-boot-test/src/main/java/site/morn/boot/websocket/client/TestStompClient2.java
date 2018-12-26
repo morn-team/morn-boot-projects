ab231088cc4ab39f7e8a2b095d0ae5aaedb99b49
timely-rain <lolic_sd@hotmail.com>
2018-12-27 00:20:17 +0800

    基于Spring的Stomp客户端Demo

---
+++ morn-boot-test/src/main/java/site/morn/boot/websocket/client/TestStompClient2.java
@@ -0,0 +1,67 @@
+package site.morn.boot.websocket.client;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.websocket.ContainerProvider;
+import javax.websocket.WebSocketContainer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+/**
+ * site.morn.boot.websocket.client
+ *
+ * @author timely-rain
+ * @since 1.0.0, 2018/12/26
+ */
+@Slf4j
+public class TestStompClient2 {
+
+  public void startup() {
+    try {
+      List<Transport> transports = new ArrayList<>(1);
+
+      WebSocketContainer webSocketContainer = ContainerProvider.getWebSocketContainer();
+      webSocketContainer.setDefaultMaxTextMessageBufferSize(50 * 1024 * 1024);
+      webSocketContainer.setDefaultMaxBinaryMessageBufferSize(50 * 1024 * 1024);
+
+      transports.add(new WebSocketTransport(new StandardWebSocketClient(webSocketContainer)));
+      WebSocketClient webSocketClient = new SockJsClient(transports);
+
+      WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+      stompClient.setInboundMessageSizeLimit(50 * 1024 * 1024);
+      stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+      stompClient.setTaskScheduler(new DefaultManagedTaskScheduler()); // for heartbeats
+
+      String url = "ws://localhost:8080/stomp/";
+      StompSessionHandler sessionHandler = new StompMessageHandler();
+      stompClient.connect(url, sessionHandler);
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+    }
+  }
+
+  public void shutdown() {
+    try {
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+    }
+  }
+
+  public class StompMessageHandler extends StompSessionHandlerAdapter {
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+      super.handleFrame(headers, payload);
+    }
+  }
+}
--- morn-boot-test/src/test/java/site/morn/boot/websocket/StompClientTest.java
+++ morn-boot-test/src/test/java/site/morn/boot/websocket/StompClientTest.java
@@ -1,6 +1,6 @@
 package site.morn.boot.websocket;
 
-import site.morn.boot.websocket.client.TestStompClient;
+import site.morn.boot.websocket.client.TestStompClient2;
 
 /**
  * site.morn.boot.websocket
@@ -11,7 +11,9 @@ import site.morn.boot.websocket.client.TestStompClient;
 public class StompClientTest {
 
   public static void main(String[] args) {
-    TestStompClient client = new TestStompClient();
-    client.startup();
+//    TestStompClient client = new TestStompClient();
+//    client.startup();
+    TestStompClient2 client2 = new TestStompClient2();
+    client2.startup();
   }
 }
