eea1ec2197f8a28cd3759774fd35e7a42e94d37f
timely-rain <lolic_sd@hotmail.com>
2018-12-27 23:41:38 +0800

    响应工具类支持构建模式

--- morn-boot-web/src/main/java/site/morn/boot/web/Responses.java
+++ morn-boot-web/src/main/java/site/morn/boot/web/Responses.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 
@@ -15,31 +18,62 @@ import org.springframework.http.MediaType;
  * @since 1.0.0, 2018/12/27
  */
 @Slf4j
+@RequiredArgsConstructor
 public final class Responses {
 
-  private Responses() {
+  /**
+   * 响应对象
+   */
+  private final ServletResponse response;
+
+  /**
+   * 获取标准响应对象
+   *
+   * @return 标准响应对象
+   */
+  public static Responses standard() {
+    HttpServletResponse response = Servlets.response();
+    if (Objects.isNull(response)) {
+      return null;
+    }
+    return standard(response);
   }
 
-  public static HttpServletResponse standard() {
-    HttpServletResponse response = Servlets.response();
+  /**
+   * 获取标准响应对象
+   *
+   * @param response 响应对象
+   * @return 标准响应对象
+   */
+  public static Responses standard(ServletResponse response) {
     response.setCharacterEncoding(StandardCharsets.UTF_8.name());
     response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-    return response;
+    return new Responses(response);
   }
 
-  public static void write(Object o) {
+  /**
+   * 应答
+   *
+   * @param object 报文
+   */
+  public void respond(Object object) {
     ObjectMapper objectMapper = new ObjectMapper();
     try {
-      String s = objectMapper.writeValueAsString(o);
-      write(s);
+      String s = objectMapper.writeValueAsString(object);
+      respond(s);
     } catch (JsonProcessingException e) {
       log.error(e.getMessage(), e);
     }
   }
 
-  public static void write(String json) {
+  /**
+   * 应答
+   *
+   * @param json 报文
+   */
+  public void respond(String json) {
     try {
-      standard().getWriter().write(json);
+      response.getWriter().write(json);
     } catch (IOException e) {
       log.error(e.getMessage(), e);
     }
