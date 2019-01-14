899bb7168ddd021d143ec4979ee5153cc44fec26
gltiana <gltiana@isoftstone.com>
2019-01-14 18:01:54 +0800

    add:JPA查询规格构建器的声明

--- morn-boot-jpa/src/main/java/site/morn/boot/jpa/SpecificationBuilder.java
+++ morn-boot-jpa/src/main/java/site/morn/boot/jpa/SpecificationBuilder.java
@@ -1,21 +1,62 @@
 package site.morn.boot.jpa;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import lombok.experimental.Accessors;
+import org.springframework.data.jpa.domain.Specification;
+
 /**
  * JPA查询规格构建器
  *
  * @author timely-rain
  * @since 1.0.0, 2019/1/13
  */
-public class SpecificationBuilder<T> {
+@Accessors(chain = true, fluent = true)
+public final class SpecificationBuilder<T, A> {
 
-//  /**
-//   * 构建JPA查询规格
-//   *
-//   * @return JPA查询规格
-//   */
-//  public Specification<T> build(){
-//    return ((root, query, cb, jc) ->{
-//
-//    });
-//  }
+  private T model;
+
+  private A attach;
+
+  private Root<T> root;
+
+  private CriteriaQuery<?> query;
+
+  private CriteriaBuilder builder;
+
+  private Predicate predicate = null;
+
+  private SpecificationBuilder() {
+  }
+
+  public static <T, A> SpecificationBuilder<T, A> builder() {
+    return new SpecificationBuilder<>();
+  }
+
+  /**
+   * 构建JPA查询规格
+   *
+   * @return JPA查询规格
+   */
+  public Specification<T> build() {
+    return ((r, cq, cb) ->
+    {
+      this.root = r;
+      this.query = cq;
+      this.builder = cb;
+      return predicate;
+    });
+  }
+
+  public SpecificationBuilder<T, A> model(T model) {
+    this.model = model;
+    return this;
+  }
+
+  public SpecificationBuilder<T, A> attach(A attach) {
+    this.attach = attach;
+    return this;
+  }
 }
