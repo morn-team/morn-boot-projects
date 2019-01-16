f1e0f2338778bcdcc6e6dd9da29080c17fb3c4d0
timely-rain <lolic_sd@hotmail.com>
2019-01-16 23:42:55 +0800

    feat:基础控制器的新增、修改、删除

--- morn-boot-support/src/main/java/site/morn/boot/support/CrudControllerSupport.java
+++ morn-boot-support/src/main/java/site/morn/boot/support/CrudControllerSupport.java
@@ -3,11 +3,18 @@ package site.morn.boot.support;
 import java.io.Serializable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import site.morn.boot.rest.RestPage;
 import site.morn.rest.RestBuilders;
 import site.morn.rest.RestMessage;
+import site.morn.rest.RestModel;
 import site.morn.util.TypeUtils;
+import site.morn.validate.group.Put;
+import site.morn.validate.group.Update;
 
 /**
  * 基础控制器实现
@@ -29,6 +36,25 @@ public class CrudControllerSupport<T, I extends Serializable, S extends CrudServ
   }
 
   /**
+   * 新增
+   */
+  @PostMapping
+  public RestMessage add(@Validated @RequestBody RestModel<T> restModel) {
+    service().add(restModel);
+    return RestBuilders.successMessage();
+  }
+
+  /**
+   * 修改
+   */
+  @PutMapping
+  public RestMessage update(
+      @Validated({Update.class, Put.class}) @RequestBody RestModel<T> restModel) {
+    service().add(restModel);
+    return RestBuilders.successMessage();
+  }
+
+  /**
    * 搜索
    *
    * @param restPage REST分页参数
@@ -38,5 +64,15 @@ public class CrudControllerSupport<T, I extends Serializable, S extends CrudServ
   public RestMessage search(RestPage<T> restPage) {
     Page<T> page = service.search(restPage);
     return RestBuilders.successMessage(page);
+  }
+
+  /**
+   * 删除
+   */
+  @DeleteMapping
+  public RestMessage delete(
+      @Validated({Update.class, Put.class}) @RequestBody RestModel<T> restModel) {
+    service().delete(restModel);
+    return RestBuilders.successMessage();
   }
 }
