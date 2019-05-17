b5f1a08e329186377a68f8cf82c0285c993a5245
timely-rain <lolic_sd@hotmail.com>
2019-05-17 22:01:56 +0800

    feat:实例增强
    - 移除Bean相关冗余接口

--- morn-core/src/main/java/site/morn/bean/AnnotationIdentify.java
+++ morn-core/src/main/java/site/morn/bean/AnnotationIdentify.java
@@ -6,6 +6,30 @@ package site.morn.bean;
  * @author timely-rain
  * @since 1.0.0, 2018/11/25
  */
-public interface AnnotationIdentify extends AnnotationName, AnnotationTag, AnnotationTarget {
+public interface AnnotationIdentify {
 
+
+  /**
+   * 获取名称
+   *
+   * @return 名称
+   * @see site.morn.bean.annotation.Name
+   */
+  String getName();
+
+  /**
+   * 获取名称标签
+   *
+   * @return 名称标签
+   * @see site.morn.bean.annotation.Tag
+   */
+  String[] getTags();
+
+  /**
+   * 获取目标
+   *
+   * @return 目标
+   * @see site.morn.bean.annotation.Target
+   */
+  Class<?> getTarget();
 }
