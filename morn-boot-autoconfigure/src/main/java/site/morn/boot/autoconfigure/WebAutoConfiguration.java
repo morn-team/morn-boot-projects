8a94aa1a60bf496f8bf027113ed5d7144a8a30d8
timely-rain <lolic_sd@hotmail.com>
2019-08-30 11:40:51 +0800

    feat:Jackson
    - 默认注册Hibernate5Module模块

--- morn-boot-autoconfigure/src/main/java/site/morn/boot/autoconfigure/WebAutoConfiguration.java
+++ morn-boot-autoconfigure/src/main/java/site/morn/boot/autoconfigure/WebAutoConfiguration.java
@@ -1,6 +1,5 @@
 package site.morn.boot.autoconfigure;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import javax.servlet.Servlet;
 import org.aspectj.lang.Aspects;
@@ -8,9 +7,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.morn.boot.web.ExceptionHandlerAspect;
@@ -55,17 +55,15 @@ public class WebAutoConfiguration {
   public static class JacksonHibernateModuleConfiguration {
 
     /**
-     * 注册Jackson消息转换器
+     * 注册Hibernate5Module模块
      *
-     * @return Jackson消息转换器
+     * @see JacksonAutoConfiguration
+     * @see Jackson2ObjectMapperBuilderCustomizer
      */
     @Bean
     @ConditionalOnMissingBean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-      MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-      ObjectMapper objectMapper = messageConverter.getObjectMapper();
-      objectMapper.registerModule(new Hibernate5Module());
-      return messageConverter;
+    public Hibernate5Module hibernate5Module() {
+      return new Hibernate5Module();
     }
   }
 }
